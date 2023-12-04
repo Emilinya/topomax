d8796b39d1496acd36d43cd8f8831e653056f592
Emilie <emilid2103@gmail.com>
2023-12-04 10:54:56 +0100

    undid changes to bridge and short_cantilever
    instead created fem_short_cantilever and dem_cantilever

--- designs/design_creator/src/dem_cantilever.rs
+++ designs/design_creator/src/dem_cantilever.rs
@@ -1,27 +1,28 @@
 use crate::definitions::*;
 
-pub fn short_cantilever() -> Design {
-    return Design::Elasticity(ProblemDesign {
+pub fn dem_cantilever() -> Design {
+    Design::Elasticity(ProblemDesign {
         objective: ElasticityObjective::MinimizeCompliance,
         domain_parameters: DomainParameters {
-            width: 10.0,
+            width: 15.0,
             height: 5.0,
-            fem_step_size: 5.0,
+            fem_step_size: 0.001,
             dem_step_size: 2.5,
             penalties: vec![3.0],
-            volume_fraction: 0.4,
+            volume_fraction: 0.5,
         },
         problem_parameters: ElasticityParameters {
             fixed_sides: vec![Side::Left],
-            body_force: None,
-            tractions: Some(vec![Traction {
-                side: Side::Right,
-                center: 2.5,
-                length: 1.0 / 9.0,
+            body_force: Some(Force {
+                region: CircularRegion {
+                    center: (14.5, 2.5),
+                    radius: 0.25,
+                },
                 value: (0.0, -2000.0),
-            }]),
+            }),
+            tractions: None,
             young_modulus: 2e5,
             poisson_ratio: 0.3,
         },
-    });
+    })
 }
--- designs/design_creator/src/fem_short_cantilever.rs
+++ designs/design_creator/src/fem_short_cantilever.rs
@@ -1,26 +1,26 @@
 use crate::definitions::*;
 
-pub fn bridge() -> Design {
+pub fn fem_short_cantilever() -> Design {
     return Design::Elasticity(ProblemDesign {
         objective: ElasticityObjective::MinimizeCompliance,
         domain_parameters: DomainParameters {
-            width: 12.0,
-            height: 2.0,
-            fem_step_size: 0.4,
-            dem_step_size: 0.4,
+            width: 2.0,
+            height: 1.0,
+            fem_step_size: 5.0,
+            dem_step_size: 2000.0,
             penalties: vec![3.0],
             volume_fraction: 0.4,
         },
         problem_parameters: ElasticityParameters {
-            fixed_sides: vec![Side::Left, Side::Right],
+            fixed_sides: vec![Side::Left],
             body_force: None,
             tractions: Some(vec![Traction {
-                side: Side::Top,
-                center: 6.0,
-                length: 0.5,
-                value: (0.0, -2000.0),
+                side: Side::Right,
+                center: 0.5,
+                length: 1.0 / 45.0,
+                value: (0.0, -1.0),
             }]),
-            young_modulus: 2e5,
+            young_modulus: 1.0,
             poisson_ratio: 0.3,
         },
     });
